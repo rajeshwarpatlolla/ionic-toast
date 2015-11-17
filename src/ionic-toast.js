f97d223d4716d60210f2eb3367f867f5dc92d4e5
zmijevik <persie.joseph@gmail.com>
2015-11-17 22:35:12 +0000

    adding info, warning, error and success messages

--- src/ionic-toast.js
+++ src/ionic-toast.js
@@ -6,7 +6,7 @@
 angular.module('ionic-toast', ['ionic'])
 
 .run(['$templateCache', function ($templateCache) {
-  var toastTemplate = '<div class="ionic_toast" ng-class="ionicToast.toastClass" ng-style="ionicToast.toastStyle">' +
+  var toastTemplate = '<div ng-class="ionicToast.toastClass" ng-style="ionicToast.toastStyle">' +
     '<span class="ionic_toast_close" ng-click="hide()"><i class="ion-close-round toast_close_icon"></i></span>' +
     '<span ng-bind-html="ionicToast.toastMessage"></span>' +
     '</div>';
@@ -61,33 +61,59 @@ angular.module('ionic-toast', ['ionic'])
         });
       };
 
+      var baseShow = function(message, position, closeBtn, duration) {
+
+        if (!message || !position || !duration) return;
+        $timeout.cancel(toastTimeout);
+        if (duration > 5000) duration = 5000;
+
+        angular.extend(toastScope.ionicToast, {
+          toastClass: toastPosition[position] + ' ionic_toast ' + (closeBtn ? 'ionic_toast_sticky' : ''),
+          toastMessage: message
+        });
+
+        toggleDisplayOfToast('block', 1, function () {
+          if (closeBtn) return;
+
+          toastTimeout = $timeout(function () {
+            toastScope.hide();
+          }, duration);
+        });
+      }
+
       return {
 
         show: function (message, position, closeBtn, duration) {
-
-          if (!message || !position || !duration) return;
-          $timeout.cancel(toastTimeout);
-          if (duration > 5000) duration = 5000;
-
+          baseShow(message, position, closeBtn, duration);
+        },
+        showError: function (message, position, closeBtn, duration) {
+          baseShow(message, position, closeBtn, duration);
           angular.extend(toastScope.ionicToast, {
-            toastClass: toastPosition[position] + ' ' + (closeBtn ? 'ionic_toast_sticky' : ''),
-            toastMessage: message
-          });
-
-          toggleDisplayOfToast('block', 1, function () {
-            if (closeBtn) return;
-
-            toastTimeout = $timeout(function () {
-              toastScope.hide();
-            }, duration);
+            toastClass: toastScope.ionicToast.toastClass + ' ionic_toast_error '
           });
         },
-
+        showWarning: function (message, position, closeBtn, duration) {
+          baseShow(message, position, closeBtn, duration);
+          angular.extend(toastScope.ionicToast, {
+            toastClass: toastScope.ionicToast.toastClass + ' ionic_toast_warning '
+          });
+        },
+        showInfo: function (message, position, closeBtn, duration) {
+          baseShow(message, position, closeBtn, duration);
+          angular.extend(toastScope.ionicToast, {
+            toastClass: toastScope.ionicToast.toastClass + ' ionic_toast_info '
+          });
+        },
+        showSuccess: function (message, position, closeBtn, duration) {
+          baseShow(message, position, closeBtn, duration);
+          angular.extend(toastScope.ionicToast, {
+            toastClass: toastScope.ionicToast.toastClass + ' ionic_toast_success '
+          });
+        },
         hide: function () {
           toastScope.hide();
         }
       };
-
     }
   ];
 });
