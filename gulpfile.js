b1d68a3060109e717af56d0e2820402d8b7a9c54
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2016-05-29 12:30:15 +0530

    gulp file indentation

--- gulpfile.js
+++ gulpfile.js
@@ -4,31 +4,31 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var css2js = require("gulp-css2js");
 
-gulp.task('build',['cssminify'], function () {
-    gulp.src(['./src/ionic-toast.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist"));
+gulp.task('build', ['cssminify'], function () {
+  gulp.src(['./src/ionic-toast.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
 });
 
 gulp.task('css2js', function () {
-    return gulp.src("./src/*.css")
-      .pipe(css2js())
-      .pipe(gulp.dest("./dist/"));
+  return gulp.src("./src/*.css")
+    .pipe(css2js())
+    .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task('del', function () {
-    del(['./dist/*']);
+  del(['./dist/*']);
 });
 
 gulp.task('make-bundle', ['del', 'css2js'], function () {
-    return gulp.src(['./dist/*', './src/*.js'])
-      .pipe(concat('ionic-toast.bundle.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('./dist/'));
+  return gulp.src(['./dist/*', './src/*.js'])
+    .pipe(concat('ionic-toast.bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('del-temp-files', ['make-bundle'], function () {
-    del(['./dist/style.js','./dist/ionic-toast.js']);
+  del(['./dist/style.js', './dist/ionic-toast.js']);
 });
 
 gulp.task('build', ['del-temp-files']);
