eb2e83fa7240e71ffdf0e5a7ad7560efbdf20d0c
dtruel <dand@turbobuilt.com>
2016-06-21 12:43:33 -0500

    add animations only tested in chrome

--- src/ionic-toast.run.js
+++ src/ionic-toast.run.js
@@ -2,7 +2,7 @@
 angular.module('ionic-toast')
 
   .run(['$templateCache', function ($templateCache) {
-    var toastTemplate = '<div class="ionic_toast" ng-class="ionicToast.toastClass" ng-style="ionicToast.toastStyle">' +
+    var toastTemplate = '<div class="ionic_toast ionic_toast_top" id="ionic_toast" ng-class="ionicToast.toastClass" ng-style="ionicToast.toastStyle">' +
       '<div class="toast_section">' +
       '<span class="ionic_toast_close" ng-click="hideToast()">' +
       '<i class="ion-android-close toast_close_icon"></i>' +
